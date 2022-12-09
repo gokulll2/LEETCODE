62a342f442232918ded4d01edd79714a002c9f26
gokulll2 <112635960+gokulll2@users.noreply.github.com>
2022-12-09 13:42:43 +0530

    Added solution - LeetHub

---
+++ Sum of Middle Elements of two sorted arrays - GFG/sum-of-middle-elements-of-two-sorted-arrays.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+public:
+    int findMidSum(int ar1[], int ar2[], int n) {
+            // code here 
+            int ar3[2*n];
+            for(int i=0;i<n;i++)
+            {
+                ar3[i]=ar1[i];
+            }
+            int k=0;
+            for(int i=n;i<2*n;i++)
+            {
+                ar3[i]=ar2[k++];
+            }
+            sort(ar3,ar3+2*n);
+            int mi=ar3[n-1];
+            int mid=ar3[n];
+            int sum=mi+mid;
+            return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int ar1[n], ar2[n];
+        for (i = 0; i < n; i++) {
+            cin >> ar1[i];
+        }
+        for (i = 0; i < n; i++) {
+            cin >> ar2[i];
+        }
+        Solution ob;
+        auto ans = ob.findMidSum(ar1, ar2, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends

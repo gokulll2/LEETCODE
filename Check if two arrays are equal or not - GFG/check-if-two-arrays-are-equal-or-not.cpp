56e3852cd5d16a1999bbfc760961f42a7ef5fa7d
gokulll2 <112635960+gokulll2@users.noreply.github.com>
2023-02-02 13:59:25 +0530

    Added solution - LeetHub

---
+++ Check if two arrays are equal or not - GFG/check-if-two-arrays-are-equal-or-not.cpp
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+
+    //Function to check if two arrays are equal or not.
+    bool check(vector<ll> A, vector<ll> B, int N) {
+        //code here
+        unordered_map<int,int>mp1;
+        unordered_map<int,int>mp2;
+        for(int i=0;i<N;i++)
+        {
+            mp1[A[i]]++;
+        }
+        for(int i=0;i<N;i++)
+        {
+            mp2[B[i]]++;
+        }
+        for(int i=0;i<N;i++)
+        {
+            if(mp2.count(A[i])==true && mp1[A[i]]==mp2[A[i]])
+            {
+                continue;
+            }
+            else{
+                return false;
+            }
+        }
+        for(int i=0;i<N;i++)
+        {
+            if(mp1.count(B[i])==true && mp1[B[i]]==mp2[B[i]])
+            {
+                continue;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+        // for(auto it:mp)
+        // {
+        //     if(mp.size()==N)
+        //     {
+        //         return true;
+        //     }
+        //      else if(it.second>=2)
+        //     {
+        //         return false;
+        //     }
+        // }
+        // return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        
+        vector<ll> arr(n,0),brr(n,0);
+        
+        // increase the count of elements in first array
+        for(ll i=0;i<n;i++)
+            cin >> arr[i];
+        
+        
+        // iterate through another array
+        // and decrement the count of elements
+        // in the map in which frequency of elements
+        // is stored for first array
+        for(ll i=0;i<n;i++)
+            cin >> brr[i];
+        Solution ob;
+        cout << ob.check(arr,brr,n) << "\n";
+    }
+	return 0;
+}
+// } Driver Code Ends

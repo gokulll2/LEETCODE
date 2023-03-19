a6d1ca6c6bab3dac3b4d56de83f0443776398080
gokulll2 <112635960+gokulll2@users.noreply.github.com>
2023-03-19 16:06:50 +0530

    Added solution - LeetHub

--- Pythagorean Triplet - GFG/pythagorean-triplet.cpp
+++ Pythagorean Triplet - GFG/pythagorean-triplet.cpp
@@ -11,35 +11,72 @@ public:
 	// Pythagorean triplet exists or not
 	bool checkTriplet(int arr[], int n) {
 	    // code here
-	    if(n<3)
-	    {
-	        return false;
-	    }
-	    sort(arr,arr+n);
-	    for(int i=0;i<n;i++)
-	    {
-	        arr[i]=arr[i]*arr[i];
-	    }
-	    for(int i=n-1;i>=0;i--)
-	    {
-	        int j=0;
-	        int r=i-1;
-	        while(j<r)
-	        {
-	            if(arr[j]+arr[r]==arr[i])
-	            {
-	                return 1;
-	            }
-	            if(arr[j]+arr[r]<arr[i])
-	            {
-	                j++;
-	            }
-	            else{
-	                r--;
-	            }
-	        }
-	    }
-	    return 0;
+	   // if(n<3)
+	   // {
+	   //     return false;
+	   // }
+	   // sort(arr,arr+n);
+	   // for(int i=0;i<n;i++)
+	   // {
+	   //     arr[i]=arr[i]*arr[i];
+	   // }
+	   // for(int i=n-1;i>=0;i--)
+	   // {
+	   //     int j=0;
+	   //     int r=i-1;
+	   //     while(j<r)
+	   //     {
+	   //         if(arr[j]+arr[r]==arr[i])
+	   //         {
+	   //             return 1;
+	   //         }
+	   //         if(arr[j]+arr[r]<arr[i])
+	   //         {
+	   //             j++;
+	   //         }
+	   //         else{
+	   //             r--;
+	   //         }
+	   //     }
+	   // }
+	   // return 0;
+	   
+	   
+	   
+	   if(n<3)
+	   {
+	       return false;
+	   }
+	   for(int i=0;i<n;i++)
+	   {
+	       arr[i]= arr[i]*arr[i];
+	   }
+	   sort(arr,arr+n);
+	   for(int i=n-1;i>=0;i--)
+	   {
+	       int j=0;
+	       int r=i-1;
+	       while(j<r)
+	       {
+	       if(arr[r]+arr[j]==arr[i])
+	       {
+	           return 1;
+	       }
+	       else if(arr[r]+arr[j]>arr[i])
+	       {
+	           r--;
+	       }
+	       else{
+	           j++;
+	       }
+	       
+	   }
+	       
+	   }
+	   return false;
+	   
+	   
+	   
 	   // for(int i=n-1;i>=0;i--)
 	   // {
 	   //     int j=0;

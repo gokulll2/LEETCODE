d766df050daedccf43889538377c0e135635a76d
gokulll2 <112635960+gokulll2@users.noreply.github.com>
2023-04-05 15:39:37 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -4,52 +4,53 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-  private:
+  public:
     // Function to find the number of islands.
-    void bfs(vector<vector<char>>&grid,int row,int col,vector<vector<int>>&vis)
+    void bfs(int row,int col,vector<vector<char>>&grid,vector<vector<int>>&vis)
     {
         vis[row][col]=1;
         queue<pair<int,int>>q;
         q.push({row,col});
-         int n=grid.size();
+        int n=grid.size();
         int m=grid[0].size();
         while(!q.empty())
         {
             int row=q.front().first;
             int col=q.front().second;
             q.pop();
-            //traverse in the neighbours and mark them as 1 if its a land.
+            
+            //traversing the neighbours if its water or land
+            
             for(int delrow=-1;delrow<=1;delrow++)
             {
                 for(int delcol=-1;delcol<=1;delcol++)
                 {
-                    int nrow=row+delrow;
-                    int ncol=col+delcol;
-                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] &&
-                     grid[nrow][ncol]=='1')
-                     {
-                         vis[nrow][ncol]=1;
-                         q.push({nrow,ncol});
-                     }
+                    int nrow=delrow+row;
+                    int ncol=delcol+col;
+                     if(nrow>=0 && nrow<n && ncol>=0 &&ncol<m && grid[nrow][ncol]=='1' && vis[nrow][ncol]==0)
+            {
+                vis[nrow][ncol]=1;
+                q.push({nrow,ncol});
+            }
                 }
+                
             }
         }
     }
-    public:
     int numIslands(vector<vector<char>>& grid) {
         // Code here
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
         int cnt=0;
-        for(int row=0;row<n;row++)
+        for(int i=0;i<n;i++)
         {
-            for(int col=0;col<m;col++)
+            for(int j=0;j<m;j++)
             {
-                if(!vis[row][col] && grid[row][col]=='1')
+                if(!vis[i][j] && grid[i][j]=='1')
                 {
                     cnt++;
-                    bfs(grid,row,col,vis);
+                    bfs(i,j,grid,vis);
                 }
             }
         }

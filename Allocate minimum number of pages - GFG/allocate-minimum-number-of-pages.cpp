3b6a16fdb764b582a582c30197b27f745aa1fe11
Goku <112635960+gokulll2@users.noreply.github.com>
2023-08-13 17:22:40 +0530

    Added solution - LeetHub

--- Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.cpp
+++ Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.cpp
@@ -12,54 +12,49 @@ class Solution
 {
     public:
     //Function to find minimum number of pages.
-    bool ispossible(int A[],int N,int M,int mid)
+    bool isPossible(int A[] , int N ,int M , int mid)
     {
-        int studentcount=1;
-        int pagesum=0;
+        int studentCount = 1;
+        int pageSum = 0;
         for(int i=0;i<N;i++)
         {
-            if(pagesum+A[i]<=mid)
+            if(pageSum+A[i]<=mid)
             {
-                pagesum+=A[i];
+                pageSum+=A[i];
             }
             else{
-                studentcount++;
-                if(studentcount>M || A[i]>mid)
+                studentCount++;
+                if(studentCount>M || A[i]>mid)
                 {
                     return false;
                 }
-                pagesum=A[i];
+                pageSum = A[i];
             }
-            // if(studentcount>M)
-            // {
-            //     return false;
-            // }
         }
         return true;
     }
     int findPages(int A[], int N, int M) 
     {
         //code here
-        //THIS CONDITION
         if(M>N)
         {
             return -1;
         }
-        int s=A[0];
+        int s = A[N-1];
         int sum=0;
         for(int i=0;i<N;i++)
         {
             sum+=A[i];
         }
-        int e=sum;
-        int mid=s+(e-s)/2;
-        int temp=-1;
+        int temp = -1;
+        int e = sum;
+        int mid = s+(e-s)/2;
         while(s<=e)
         {
-            if(ispossible(A,N,M,mid))
+            if(isPossible(A,N,M,mid))
             {
-                temp=mid;
-                e=mid-1;
+                temp = mid;
+                e = mid-1;
             }
             else{
                 s=mid+1;
@@ -67,6 +62,7 @@ class Solution
             mid=s+(e-s)/2;
         }
         return temp;
+        
     }
 };
 
